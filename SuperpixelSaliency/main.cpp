45d9cd660e4605caa2320279ce463c2452778a2f
takayama <macho4287@gmail.com>
2015-06-26 14:07:40 +0900

    Initial Commit

---
+++ SuperpixelSaliency/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SuperpixelSaliency
+//
+//  Created by Takayama Shota on 6/26/15.
+//  Copyright (c) 2015 Takayama Shota. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
